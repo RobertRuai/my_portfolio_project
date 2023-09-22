e1f2d347e6121e537a5fd56a9041bc26608a1eb5
Robert Ruai Mawich <robertruai4@gmail.com>
2023-09-22 12:29:21 +0300

    minor edits

--- crime_report/app.py
+++ crime_report/app.py
@@ -2,28 +2,29 @@
 
 from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, InputRequired, Length, EqualTo
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from db import db
 from models.base_model import CrimeRecord
 from config import SECRET_KEY
 from flask_bcrypt import Bcrypt
+from config import MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+db = SQLAlchemy()
+bcrypt = Bcrypt(app)
 app.config['SECRET_KEY'] = SECRET_KEY
-
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:///{}:{}@{}/{}'.format(MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_DB)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crime_report.db'
 # Configure database
-db.init_app(app)
 
 # Configure login manager
 login_manager = LoginManager()
-login_manager.login_view = 'login'
 login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-class User(UserMixin):
-    pass
 
 #another example of user class db
 class User(db.Model, UserMixin):
@@ -70,46 +71,47 @@ def load_user(user_id):
     user.id = user_id
     return user
 
-class CrimeRecordForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    description = TextAreaField('Description', validators=[DataRequired()])
+#class CrimeRecordForm(FlaskForm):
+    #title = StringField('Title', validators=[DataRequired()])
+    #description = TextAreaField('Description', validators=[DataRequired()])
 
 @app.route('/')
 @login_required
 def home():
-    crime_records = CrimeRecord.query.all()
-    return render_template('index.html', crime_records=crime_records)
+    return render_template('./user/home.html')
+    #crime_records = CrimeRecord.query.all()
+    #return render_template('index.html', crime_records=crime_records)
 
-@app.route('/add', methods=['GET', 'POST'])
-@login_required
-def add_record():
-    form = CrimeRecordForm()
-    if form.validate_on_submit():
-        new_record = CrimeRecord(title=form.title.data, description=form.description.data)
-        db.session.add(new_record)
-        db.session.commit()
-        return redirect(url_for('home'))
-    return render_template('add_record.html', form=form)
+#@app.route('/add', methods=['GET', 'POST'])
+#@login_required
+#def add_record():
+    #form = CrimeRecordForm()
+    #if form.validate_on_submit():
+        #new_record = CrimeRecord(title=form.title.data, description=form.description.data)
+        #db.session.add(new_record)
+        #db.session.commit()
+        #return redirect(url_for('home'))
+    #return render_template('add_record.html', form=form)
 
-@app.route('/edit/<int:record_id>', methods=['GET', 'POST'])
-@login_required
-def edit_record(record_id):
-    record = CrimeRecord.query.get(record_id)
-    form = CrimeRecordForm(obj=record)
-    if form.validate_on_submit():
-        record.title = form.title.data
-        record.description = form.description.data
-        db.session.commit()
-        return redirect(url_for('home'))
-    return render_template('edit_record.html', form=form, record_id=record_id)
+#@app.route('/edit/<int:record_id>', methods=['GET', 'POST'])
+#@login_required
+#def edit_record(record_id):
+    #record = CrimeRecord.query.get(record_id)
+    #form = CrimeRecordForm(obj=record)
+    #if form.validate_on_submit():
+        #record.title = form.title.data
+        #record.description = form.description.data
+        #db.session.commit()
+        #return redirect(url_for('home'))
+    #return render_template('edit_record.html', form=form, record_id=record_id)
 
-@app.route('/delete/<int:record_id>')
-@login_required
-def delete_record(record_id):
-    record = CrimeRecord.query.get(record_id)
-    db.session.delete(record)
-    db.session.commit()
-    return redirect(url_for('home'))
+#@app.route('/delete/<int:record_id>')
+#@login_required
+#def delete_record(record_id):
+    #record = CrimeRecord.query.get(record_id)
+    #db.session.delete(record)
+    #db.session.commit()
+    #return redirect(url_for('home'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -120,13 +122,13 @@ def login():
             if bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
                 return redirect(url_for('dashboard'))
-    return render_template('login.html', form=form)
+    return render_template('./user/login.html', form=form)
 
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template('./user/dashboard.html')
 
 
 @app.route('/logout', methods=['GET', 'POST'])
@@ -147,7 +149,7 @@ def register():
         db.session.commit()
         return redirect(url_for('login'))
 
-    return render_template('register.html', form=form)
+    return render_template('./user/register.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- crime_report/models/base_model.py
+++ crime_report/models/base_model.py
@@ -1,5 +1,8 @@
-from db import db
+#!/usr/bin/python3
+from flask_sqlalchemy import SQLAlchemy
 
+
+db = SQLAlchemy()
 class CrimeRecord(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
